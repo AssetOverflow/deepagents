a85d3d8f7c0ac85656636317b530aafd1545054d
Shay <assetoverflow@icloud.com>
2026-06-26 04:33:13 -0700

    feat: export governed deep agent factory

--- libs/deepagents/deepagents/__init__.py
+++ libs/deepagents/deepagents/__init__.py
@@ -1,7 +1,16 @@
 """DeepAgents package."""
 
+from deepagents.governed import DEFAULT_GOVERNED_ALLOW_TOOLS, create_governed_deep_agent
 from deepagents.graph import create_deep_agent
 from deepagents.middleware.filesystem import FilesystemMiddleware
 from deepagents.middleware.subagents import CompiledSubAgent, SubAgent, SubAgentMiddleware
 
-__all__ = ["CompiledSubAgent", "FilesystemMiddleware", "SubAgent", "SubAgentMiddleware", "create_deep_agent"]
+__all__ = [
+    "CompiledSubAgent",
+    "DEFAULT_GOVERNED_ALLOW_TOOLS",
+    "FilesystemMiddleware",
+    "SubAgent",
+    "SubAgentMiddleware",
+    "create_deep_agent",
+    "create_governed_deep_agent",
+]

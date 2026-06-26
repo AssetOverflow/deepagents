ef9c8b77d0d6d77c9e2fc2903fb0bc1f2e3b1ac7
Shay <assetoverflow@icloud.com>
2026-06-26 03:32:10 -0700

    feat(backends): export read-only filesystem backend

--- libs/deepagents/deepagents/backends/__init__.py
+++ libs/deepagents/deepagents/backends/__init__.py
@@ -3,6 +3,7 @@
 from deepagents.backends.composite import CompositeBackend
 from deepagents.backends.filesystem import FilesystemBackend
 from deepagents.backends.protocol import BackendProtocol
+from deepagents.backends.readonly_filesystem import ReadOnlyFilesystemBackend
 from deepagents.backends.sandbox import BaseSandbox, GrpcSandbox
 from deepagents.backends.state import StateBackend
 from deepagents.backends.store import StoreBackend
@@ -13,6 +14,7 @@ __all__ = [
     "CompositeBackend",
     "FilesystemBackend",
     "GrpcSandbox",
+    "ReadOnlyFilesystemBackend",
     "StateBackend",
     "StoreBackend",
 ]
